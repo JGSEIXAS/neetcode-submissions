8e47e60afe809ca9c0cc44a9df88e205de3aecc7
JGSEIXAS <101992372+JGSEIXAS@users.noreply.github.com>
2026-08-10 21:13:29 -0300

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> resultado;
+ 
+        for(int k = 1; k< nums.size() - 1; k++){
+            int i =0;
+            int j = nums.size() - 1;
+            int target = nums[k] * -1;
+            while(i< k and j> k){
+                if(nums[i] + nums[j] < target){
+                    i++;
+                }
+                else if(nums[i] + nums[j] > target){
+                    j--;
+                }
+                else if(nums[i] + nums[j] == target){
+                    resultado.insert({nums[i], nums[k], nums[j]});
+                    i++;
+                    j--;
+                }
+            }
+        }
+            vector<vector<int>> resultados(resultado.begin(), resultado.end());
+            return resultados;
+    }
+};
